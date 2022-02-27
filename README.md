5e0fb8d9be20f3f3adcd9213e24c9ec425c97861
Lotte <lottejd@gmail.com>
2022-02-27 16:26:56 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Othello
+
+## Ideer til eval
+
+- Gå igennem kommende spil
+- Hjørner og kanter er mere værd
+- Minimax med sort = max, hvid = min
+
+## Ideer til cut-off
+
+- Depth (ved ikke hvor stort)
--- SmarterAI.java
+++ SmarterAI.java
@@ -19,6 +19,7 @@ public class SmarterAI implements IOthelloAI {
         }
 
         if(bestPos != null){
+            System.out.println("Best move: " + bestEval);
             return bestPos;
         }
 
@@ -28,10 +29,19 @@ public class SmarterAI implements IOthelloAI {
     private int eval(Position p, GameState s) {
 		GameState tmpState = new GameState(s.getBoard(), s.getPlayerInTurn());
 		tmpState.insertToken(p);
+        tmpState.changePlayer();
         return tmpState.countTokens()[tmpState.getPlayerInTurn()-1];
 	}
 
-	private boolean isCutoff() {
-		return false;
+
+    private int recEval(int depth, Position p, GameState s) {
+        if (isCutoff(depth)) return eval(p, s);
+        // Recursively go through all possible moves from position p, then recursively go through all moves from 'p... Until cutoff is met
+        // Corners > sides > normal positions
+        return 0;
+    }
+
+	private boolean isCutoff(int depth) {
+		return depth > 4;
 	}
 }
