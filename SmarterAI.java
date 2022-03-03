2d30b7c0f3e90f27d7a89347d2592631f4521fb2
KalleWil <kalle-wil@hotmail.com>
2022-03-03 14:12:16 +0100

    Updated SmarterAI with MiniMax algorithm(still bugged)
    Co-authored-by: lauritsbonde <lauritsbonde@users.noreply.github.com>
    Co-authored-by: Markus Johansen <KusMar00@users.noreply.github.com>
    Co-authored-by: lottejd <lottejd@users.noreply.github.com>

--- SmarterAI.java
+++ SmarterAI.java
@@ -5,43 +5,70 @@ import java.util.ArrayList;
  */
 public class SmarterAI implements IOthelloAI {
 
-	public Position decideMove(GameState s) {
-        ArrayList<Position> moves = s.legalMoves();
+    private final int CUTOFF = 5;
 
-        Position bestPos = null;
-        int bestEval = 0;
-        for (Position position : moves) {
-            int curEval = eval(position, s);
-            if (curEval >= bestEval) { 
-                bestEval = curEval;
-                bestPos = position;
-            }
-        }
-
-        if(bestPos != null){
-            System.out.println("Best move: " + bestEval);
-            return bestPos;
-        }
-
-		return null;
+	public Position decideMove(GameState s) {   // MiniMax search
+        return maxValue(0, s).getMove();
 	}
     
-    private int eval(Position p, GameState s) {
-		GameState tmpState = new GameState(s.getBoard(), s.getPlayerInTurn());
-		tmpState.insertToken(p);
-        tmpState.changePlayer();
-        return tmpState.countTokens()[tmpState.getPlayerInTurn()-1];
+    private UtilityMove eval(GameState s) {
+        int acquiredTokens = s.getPlayerInTurn() == 1 ? s.countTokens()[0] : -s.countTokens()[1];
+        return new UtilityMove(acquiredTokens, null);
 	}
 
-
-    private int recEval(int depth, Position p, GameState s) {
-        if (isCutoff(depth)) return eval(p, s);
-        // Recursively go through all possible moves from position p, then recursively go through all moves from 'p... Until cutoff is met
-        // Corners > sides > normal positions
-        return 0;
+    private UtilityMove maxValue(int depth, GameState s) {
+        if (isCutoff(depth,s)) return eval(s);
+        
+        UtilityMove bestMove = new UtilityMove(Integer.MIN_VALUE, null);
+        for (Position p : s.legalMoves()) {
+            System.out.println("maxpositions is null: " + (p == null));
+            UtilityMove currentMove = minValue(depth+1, futureState(p, s));
+            if(currentMove.utility > bestMove.utility){
+                bestMove = currentMove;
+            }
+        }
+        return bestMove;
     }
 
-	private boolean isCutoff(int depth) {
-		return depth > 4;
+    private UtilityMove minValue(int depth, GameState s) {
+        if (isCutoff(depth,s)) return eval(s);
+        
+        UtilityMove bestMove = new UtilityMove(Integer.MAX_VALUE, null);
+        for (Position p : s.legalMoves()) {
+            System.out.println("minposition is null: " + (p == null));
+            UtilityMove currentMove = maxValue(depth+1, futureState(p, s));
+            if(currentMove.utility < bestMove.utility){
+                bestMove = currentMove;
+            }
+        }
+        return bestMove;
+    }
+
+	private boolean isCutoff(int depth, GameState s) {
+        return s.isFinished() || depth > CUTOFF; // Returns if we have reached a terminalstate or our cutoff constant.
 	}
+
+    private GameState futureState(Position p, GameState s){
+        GameState tmpState = new GameState(s.getBoard(), s.getPlayerInTurn());
+		tmpState.insertToken(p);
+        return tmpState;
+    }
+}
+
+class UtilityMove {
+    int utility;
+    Position move;
+
+    public UtilityMove(int utility, Position move){
+        this.utility = utility;
+        this.move = move;
+    }
+    
+    public int getUtility(){
+        return utility;
+    }
+
+    public Position getMove(){
+        return move;
+    }
 }
